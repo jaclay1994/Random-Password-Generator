aa4f5ca1a4130aae3cd6c0ed63d1b545a286ec73
jaclay1994 <jaclay94@gmail.com>
2021-01-07 23:39:22 -0500

    Last commit to update code.

---
+++ README.md
@@ -0,0 +1,13 @@
+### Random Password Generator
+
+This activity was designed to teach the ability to create javascript code to generate a random password
+with 8 to 128 characters
+
+### Progress
+
+This program is not able to produce a password with 8 to 128 characters. Through one cycle it is able to distinguish what kinds of 
+values to produce however. The if functions in the writePassword function are able to remove the designate values from the charset
+variable that will determine the values used in the password. I could have definitely spent more time on this activity however I got caught up in the break and did not expect to have as much trouble as I did. However I was able to learn a great deal about javascript from this 
+activity. I believe if I could pull the value from the prompt that asks for the number of characters and multiple that by the loop, then I might have something that functions at least as a basic password generate. I would like work on this some more to see if I can get it to work or even see where I went wrong and what I am missing. 
+
+I will definitely need to do more research on attributes and javascript functions. 
--- script.js
+++ script.js
@@ -1,33 +1,76 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var getStuff = document.querySelector("#Your-Secure-Password")
+var passwordText = document.querySelector("#password")
 
+
+//Variables
+var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWUXYZ1234567890 !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+var passwordGen = ""
+
+//Password Function only generates one character at a time.`-\*_*/-`
 function writePassword() {
-  //var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  var password = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWUXYZ1234567890 !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  var passwordLength = prompt("How many characters do you want in this passord? (8 to 128 characters)")
+  if (passwordLength < 8 || passwordLength > 128) {
+    writePassword();
+  }
+
+  var confirmSymbols = confirm("Do you want symbols in your password?");
+
+  if (confirmSymbols == false) {
+
+    charset = charset.replace(" !#$%&'()*+,-./:;<=>?@[\]^_`{|}~","");
+
+  }
+
+  var confirmUpperCase = confirm("Do you want uppercase characters in your password?");
+
+  if (confirmUpperCase == false) {
+
+    charset = charset.replace("ABCDEFGHIJKLMNOPQRSTUVWUXYZ","");
+
+  }
+
+  var confirmLowerCase = confirm("Do you want lowercase characters in your password?");
+
+  if (confirmLowerCase == false) {
+
+    charset = charset.replace("abcdefghijklmnopqrstuvwxyz","");
+
+  }
+
+  var confirmNumbers = confirm("Do you want numbers in your password?");
+
+  if (confirmNumbers == false) {
+
+    charset = charset.replace("1234567890","");
+
+  }
+
+//Text generator
+  var password = generatePassword();
   passwordText.value = password;
 
-  
-
-
-
-  
-  
-
-
-//for (i = 0; i < characters.length; i++) {
-  //result += characters.charAt(Math.floor(Math.random() * charactersLength));
-//}
-
-  
 }
 
-function getRandomSymbol() {
-  const symbols = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}"
-  return symbols[Math.floor(Math.random() * symbols.length)];
+//Random Character Selector
+function generatePassword() {
+
+  for (var i = 0, n = charset.length; i < length; ++i ) {
+    passwordGen += charset.charAt(Math.floor(Math.random()* n));
+    return passwordGen
+  }
 }
-console.log(writePassword)
+
+function finalPasswordLength() {
+
+  for (var i = 8, n = passwordLength.value; i < value; ++i) {
+    passwordLength.value * generatePassword
+    return generatePassword
+  }
+}
+
+
 
 
 // Add event listener to generate button
